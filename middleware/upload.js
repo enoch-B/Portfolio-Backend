9d5ec821dbed74df1714b39cdeff23b1b379802d
enoch B <henokt828@gmail.com>
2025-12-25 20:59:09 +0300

    feat: add blog post upload configuration and export for handling blog post media

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 import { profilePictureStorage } from "../config/cloudinary.js";
+import { blogPostStorage } from "../config/cloudinary.js";
 
 const fileFilter = (req, file, cb) => {
   const allowedMimeTypes = [
@@ -22,10 +23,22 @@ export const uploadConfigs = {
       fileSize: 2 * 1024 * 1024 // 2MB
     },
     fileFilter
+  }),
+
+  blogPostUpload: multer({
+    storage: blogPostStorage,
+    limits: {
+      fileSize: 2 * 1024 * 1024 // 2MB
+    },
+    fileFilter
   })
 };
 
+
+
+
 export const uploadProfilePicture =
   uploadConfigs.profilePictureUpload.single("profilePicture");
+export const uploadBlogPost = uploadConfigs.blogPostUpload.single("blogPost");
 
 export default uploadConfigs;
